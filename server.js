1d04122361f482eb75ce23aea4a4a83ddf9d56d5
AbduMurad <abadeem@gmail.com>
2020-09-28 19:23:14 +0200

    database deployed and connected

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const image = require('./controllers/image');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl : true
+    //   host : '127.0.0.1',
+    //   user : 'postgres',
+    //   password : 'test',
+    //   database : 'smart-brain'
     }
 });
 
