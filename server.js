e8a766c42dec788a7f2c934d0f5961c3caca70bf
AbduMurad <abadeem@gmail.com>
2020-10-01 21:57:21 +0200

    so frustratedgit add .git add .

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
-        ssl : true,
+        ssl : { rejectUnauthorized: false}
         //   host : '127.0.0.1',
         //   user : 'postgres',
         //   password : 'test',
